87e6e4f220a66c47adabfeac5676eeceda2a48ef
Anuj Jamwal <anujjam@thoughtworks.com>
2013-07-11 00:24:00 +0530

    added some footer styling

--- app/models/asset_type.rb
+++ app/models/asset_type.rb
@@ -4,4 +4,12 @@ class AssetType < ActiveRecord::Base
   has_many :forms, foreign_key: :asset_type_id
   has_many :assets
   has_many :models
+
+  def unassigned_count
+    Asset.unassigned_count self.id
+  end
+
+  def total_asset_count
+    Asset.where(asset_type_id: self.id).count(:id)
+  end
 end
