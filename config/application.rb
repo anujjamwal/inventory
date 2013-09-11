38bb74f2daebe62f906e4953805ce50d0136e7a0
Anuj <anujjamwal@thoughtworks.com>
2013-09-11 17:09:08 +0530

    Added font awesome file to precompile

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Inventory
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-		config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+		config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.ttf *.eot *.woff *.ico *.otf)
 
     #Ruby cas server config
     config.rubycas.cas_base_url = 'https://cas.thoughtworks.com/cas/'
